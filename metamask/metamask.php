1e551e02bfc403854f5baaeca6d628450a1a658b
Alain Papazoglou <apapazoglou@leni.fr>
2020-11-21 18:52:31 +0100

    plugin metamask opérationnel

--- metamask/metamask.php
+++ metamask/metamask.php
@@ -8,11 +8,16 @@ Plugin Name: Metamask
 Plugin URI: https://wordpress.org/plugins/metamask23_131/
 Description: intégration metamask
 Author: Al P
-Version: 0.0.1
+Version: 0.0.2
 Author URI: https://www.kredeum.com
 */
 
+function metamask_script(){
+	printf('<script defer src="/wp-content/plugins/metamask/metamask.js"></script>' );
+}
+add_action( 'wp_head', 'metamask_script' );
+
 function metamask() {
-	printf('<li id="metamask">%s</li>', "Metamask" );
+	printf('<li id="metamask"><svelte-metamask></li>' );
 }
 add_action( 'wp_meta', 'metamask' );
